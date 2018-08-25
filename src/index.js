c96d1ff0020760bb8e55f40f43435f4afb2ab903
Amal Sudama <cds.sudama@gmail.com>
2018-08-24 20:08:50 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import SimpleStorageContract from './truffle-contracts/SimpleStorage.json'
 import Web3Load from './components/Web3Load'
 import App from './components/App'
+import SimpleStorage from './components/SimpleStorage'
 import CreateAuction from './components/Auction'
-
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
     <Web3Load dappContracts={SimpleStorageContract}>
       <Router>
-        <div>
-          <Route exact path="/" component={App} />
-          <Route path="/new" component={CreateAuction} />
+        <div className="App">
+          <ul>
+            <NavLink to="/" className="navbar pure-menu pure-menu-horizontal">
+              SimpleStorage
+            </NavLink>
+            <NavLink
+              to="/new"
+              className="navbar pure-menu pure-menu-horizontal">
+              New Auction
+            </NavLink>
+          </ul>
+
+          <main className="container">
+            <Route exact path="/" component={SimpleStorage} />
+            <Route path="/new" component={CreateAuction} />
+          </main>
         </div>
       </Router>
     </Web3Load>
   </Provider>,
   document.getElementById('root')
 )
+
+/* <a href="/" className="pure-menu-heading pure-menu-link">
+             *   Truffle x
+             * </a> */
