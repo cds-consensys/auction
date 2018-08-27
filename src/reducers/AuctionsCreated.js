49aa210ae75c13d0b0fb750c35e63fc17060270c
Amal Sudama <cds.sudama@gmail.com>
2018-08-26 23:10:54 -0400

    implement auction_created reducer

--- src/reducers/AuctionsCreated.js
+++ src/reducers/AuctionsCreated.js
@@ -2,25 +2,12 @@ import { AUCTION_CREATED, AUCTIONS_LOADED } from '../actions/types'
 
 const initialState = {}
 
-/*
- *     return {
- *       beneficiary,
- *       auctionInstance: auction,
- *       startTime: new Date(startTime.c * 1000),
- *       endTime: new Date(endTime.c * 1000),
- *       itemName,
- *       itemDescription,
- *       ipfsHash,
- *       isMyAuction
- *     }
- **/
-
 export default (state = initialState, action) => {
-  const { me, actions, type, address, beneficiary, endTime, auctions } = action
-
-  if (type === AUCTION_CREATED) {
-    return state
-  } else if (type === AUCTIONS_LOADED) {
+  if (action.type === AUCTION_CREATED) {
+    const { address: summary } = action
+    return { ...state, [summary.auctionInstance.address]: summary }
+  } else if (action.type === AUCTIONS_LOADED) {
+    const { auctions } = action
     return auctions.reduce((pre, cur) => {
       const { auctionInstance } = cur
       pre[auctionInstance.address] = cur
