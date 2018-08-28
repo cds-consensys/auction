997e462eef30b18e2cf63141bbf50bc90a0efe69
Amal Sudama <cds.sudama@gmail.com>
2018-08-27 21:37:06 -0400

    make navbar stateful to display Metamask account

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,37 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default () => (
-  <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li>
-          <NavLink className="nav-item nav-link active" to="/new">
-            Create Auction
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="nav-item nav-link" to="/list">
-            List Auctions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="nav-item nav-link" to="/test">
-            Examples
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li>
+              <NavLink className="nav-item nav-link active" to="/new">
+                Create Auction
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-item nav-link" to="/list">
+                List Auctions
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-item nav-link" to="#">
+                {this.props.account}
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  account: state.accounts[0]
+})
+
+export default connect(mapStateToProps)(NavBar)
